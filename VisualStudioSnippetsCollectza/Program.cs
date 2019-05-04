f2f37c0864d89a94f58c8f83ac7e4bc7671cf96a
于瀚杰 <net_zhuyuzi@hotmail.com>
2019-05-04 10:01:16 +0800

    添加项目文件。

---
+++ VisualStudioSnippetsCollectza/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VisualStudioSnippetsCollectza {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+      
+    }
+   
+  }
+}
